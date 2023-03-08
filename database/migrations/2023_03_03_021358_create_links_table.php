6b40590b88a646e9279ae4a860336747d2b5cde6
Ali Özen <ali.ozen@nic.tr>
2023-03-08 03:58:30 +0100

    removed comment line

--- database/migrations/2023_03_03_021358_create_links_table.php
+++ database/migrations/2023_03_03_021358_create_links_table.php
@@ -10,8 +10,6 @@ return new class () extends Migration {
         Schema::create('links', function (Blueprint $table): void {
             $table->foreignUlid('project_id')->index()->constrained()->cascadeOnDelete();
             $table->string('name');
-
-//            $table->foreign('project_id')->references('id')->on('projects');
         });
     }
 
--- database/migrations/2023_03_03_141802_create_project_tag_table.php
+++ database/migrations/2023_03_03_141802_create_project_tag_table.php
@@ -10,9 +10,6 @@ return new class () extends Migration {
         Schema::create('project_tag', function (Blueprint $table): void {
             $table->foreignUlid('project_id')->index()->constrained()->cascadeOnDelete();
             $table->foreignUlid('tag_id')->index()->constrained()->cascadeOnDelete();
-
-//            $table->foreign('project_id')->references('id')->on('projects');
-//            $table->foreign('tag_id')->references('id')->on('tags');
         });
     }
 

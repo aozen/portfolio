4378304f85f35b9c7e210c60b688c6ce7b7c9564
Ali Özen <ali.ozen@nic.tr>
2023-03-16 17:40:25 +0100

    added favicon (without an image)

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/logo/logo.png') }}">
     @vite(['resources/js/app.js', 'resources/css/app.css'])
     <title>{{ $info['full_name'] }} | {{ $info['title'] }} @yield('title')</title>
     @yield('css')

0d984991349d4349e088dd0c99575deafa1d71b3
Ali Özen <ali.ozen@nic.tr>
2023-04-02 19:42:01 +0200

    added swiper to portfolio page (WIP)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+Swiper.use([Navigation, Pagination]);
+
+let portfolio_swiper = document.querySelectorAll('.swiper');
+
+if (portfolio_swiper.length) {
+    portfolio_swiper.forEach(function (swiper) {
+        let swiperId = swiper.getAttribute('id');
+        new Swiper('#' + swiperId, {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            allowTouchMove: true,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+    });
+}
+
 import './bootstrap';

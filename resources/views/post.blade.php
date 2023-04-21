6c8e9c658ff358a4eed0fac9e3afec82cc5de3ba
Ali Özen <ali.ozen@nic.tr>
2023-04-21 12:29:40 +0200

    height changed 60 to 96 on post images

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -13,9 +13,9 @@
     </div>
     <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 border-t pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3" style="border-color: {{ $post->category->color }}"></div>
     @if(count($post->images) > 0)
-        <div class="bg-contain bg-center bg-no-repeat min-h-full h-60" style="background-image: url('{{ asset("storage/" . $post->images->random()->path) }}')"></div>
+        <div class="bg-contain bg-center bg-no-repeat min-h-full h-96" style="background-image: url('{{ asset("storage/" . $post->images->random()->path) }}')"></div>
     @else
-        <div class="bg-contain bg-cover bg-no-repeat min-h-full h-60" style="background-image: url('{{ asset("images/logo/default-banner-full.jpg") }}')"></div>
+        <div class="bg-contain bg-cover bg-no-repeat min-h-full h-96" style="background-image: url('{{ asset("images/logo/default-banner-full.jpg") }}')"></div>
     @endif
     <div class="mx-auto max-w-7xl px-6 lg:px-8 mt-3 mb-10 sm:mb-16 ">{!! $post->text !!}</div>
 @endsection

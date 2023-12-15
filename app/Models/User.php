56dad15a3bd953366700733ec1044a02b489b523
Ali Ozen <aliozendev@gmail.com>
2023-12-15 12:22:38 +0100

    added laravel pulse

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isAdmin(): bool
+    {
+        return $this->is_admin == 1;
+    }
 }
